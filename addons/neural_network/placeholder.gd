eedc82c1d7619ff811a2476ad9323681a730bead
ZeroLikviz <143567019+ZeroLikviz@users.noreply.github.com>
2023-12-23 00:37:51 +0300

    Update placeholder.gd

--- addons/neural_network/placeholder.gd
+++ addons/neural_network/placeholder.gd
@@ -1,2 +1,6 @@
 @tool
 extends EditorPlugin
+
+func _enter_tree() -> void:
+	if not DirAccess.dir_exists_absolute("res://addons/neural_network/data/"):
+		DirAccess.make_dir_absolute("res://addons/neural_network/data")
