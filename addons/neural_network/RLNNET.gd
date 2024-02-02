01f2126763d391b26e90db0c8cefc48c0ab6b5ac
ZeroLikviz <143567019+ZeroLikviz@users.noreply.github.com>
2024-02-02 23:08:04 +0300

    Deleted description
    Deleted description I will replace with new one

--- addons/neural_network/RLNNET.gd
+++ addons/neural_network/RLNNET.gd
@@ -1,40 +1,19 @@
-## class used for training basic neural networks using reinforcement learning
-##
-## provides functional to train neural network using reinforcement learning. [br]
-## to do that you need only 4 functions: [br]
-## - [method RLNNET.set_reward] [br]
-## - [method RLNNET.update] [br]
-## - [method RLNNET.get_output] [br]
-## - [method RLNNET.set_input] [br]
-## [br]
-## [param set_input()] using it you giving neural_network current state of enviroment [br]
-## [param get_output()] it lets you to get what neural_network thinks [br]
-## [param set_reward()] using it you say how good or bad current approach was [br]
-## [param update()] it uses the reward to change current approach and make a better one. After calling this function reset the enviroment [br]
-
 @icon("res://addons/neural_network/RLNNET icon.png")
 class_name RLNNET
 
-## contains main neural_network, you can think about it like main or stable approach
 var main : NNET = NNET.new([1,1],1.0,false)
-## contains extra neural_network, which used for testing new approaches
 var buffer : NNET = NNET.new([1,1],1.0,false)
-## reward
 var reward : float = 0.0
-## extra container for reward
 var reward_buffer : float = 0.0
-## indicates if updated function has been called
 var has_updated : bool = false
-## used for avoiding unnecessary runs, basically it is used for optimization
 var has_runned : bool = false
-## curiosity (mutation) rate, essentially it is how much different new approaches will be from main approach. I recommend to use small values like 0.0005
 var curiosity_rate : float = 0.1
+
 func _init(layers_construction: Array = [1,1], curiosity_rate_a: float = 0.008, use_bias: bool = true, range_a: NNET.TheRange = NNET.TheRange.new(0.0,1.0), afd_a : bool = false) -> void:
 	main.assign(NNET.new(layers_construction, 0.1, use_bias, range_a, afd_a))
 	buffer.assign(main.duplicate())
 	curiosity_rate = curiosity_rate_a
 
-## creates new approach. You shouldn't use this function, forget about its existence. Using it without necessary knowledges can break everything down
 func mutate() -> void:
 	has_runned = false
 	var i : int = 0
@@ -48,13 +27,11 @@ func mutate() -> void:
 			buffer.biases[layer][i] += randf_range(-curiosity_rate, curiosity_rate)
 			i += 1
 
-## sets input
 func set_input(input : Array[float]) -> void:
 	main.set_input(input.duplicate(true))
 	buffer.set_input(input.duplicate(true))
 	has_runned = false
 
-## runs neural network
 func run() -> void:
 	if has_updated:
 		buffer.run()
@@ -62,7 +39,6 @@ func run() -> void:
 		main.run()
 	has_runned = true
 
-## returns output of new approach. If new approach doesn't exist, it will return output of main approach
 func get_output() -> Array:
 	if not has_runned:
 		run()
@@ -70,20 +46,16 @@ func get_output() -> Array:
 		return buffer.get_output()
 	return main.get_output()
 
-## generates output using main approach [member RLNNET.main]
 func get_main_output() -> Array:
 	main.run()
 	return main.get_output()
 
-## sets reward for current approach. Reward is used to calculate if new approach was better or worse
 func set_reward(value : float) -> void:
 	if not has_updated:
 		reward = value
 		return
 	reward_buffer = value
 
-## decides if new approach was worse or better, and creates new one. [br]
-## you should provide reward to [method RLNNER.set_reward] before calling this function
 func update() -> void:
 	has_updated = true
 	if reward_buffer > reward:
@@ -96,14 +68,12 @@ func update() -> void:
 	buffer.assign(main)
 	mutate()
 
-## use it when you change initial environment
 func reset() -> void:
 	has_updated = false
 	has_runned = false
 	reward = 0.0
 	reward_buffer = -1.0
 
-## shows result of new approach. If new approach doesn't exist, it will return result of main approach
 func print_output() -> void:
 	if not has_runned:
 		run()
@@ -112,9 +82,8 @@ func print_output() -> void:
 		return
 	main.print_output()
 
-## returns copy of [member RLNNET.main]
 func get_main() -> NNET:
 	return main.duplicate()
-## sets [member RLNNET.curiosity_rate]
+
 func set_curiosity_rate(rate : float) -> void:
 	curiosity_rate = rate
