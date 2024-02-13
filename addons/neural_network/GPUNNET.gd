7bcfca4425094c2d09c8927b6ef5b60d70c880da
LikVis <143567019+ZeroLikviz@users.noreply.github.com>
2024-02-13 21:16:53 +0300

    fix
    adapted this to work with GPUNNET.glsl

--- addons/neural_network/GPUNNET.gd
+++ addons/neural_network/GPUNNET.gd
@@ -1,7 +1,7 @@
 @icon("res://addons/neural_network/GPUNNET icon.png")
 class_name GPUNNET
 
-enum {run = 0, find_deltas = 1, train = 2, fill_table = 3}
+enum {run = 0, find_deltas = 1, train = 2}
 
 var device                 : RenderingDevice
 
@@ -19,26 +19,19 @@ var learning_rate : float
 var layers : Array[int]
 var last_layer : int
 var layers_size : int
+var is_bias_used : bool
 
 func _init(layers_construction: Array[int] = [1,1], learning_rate_value: float = 1.0, use_bias: bool = true) -> void:
 	learning_rate = learning_rate_value
 	last_layer    = layers_construction.size() - 1
 	layers_size   = layers_construction.size()
 	layers        = layers_construction
+	is_bias_used = use_bias
 	assert(layers_size >= 2, "structure of neural network should have at least 2 layers") 
 	var i : int = 0
 	while i < layers_size:
 		assert(layers[i] >= 1, "layer size should be equal or more than 1") 
 		i += 1
-	
-	var bias_size : int = 0;    i = 1
-	while i < layers_size:
-		bias_size += layers[i]
-		i += 1
-	var weights_size : int = 0; i = 0
-	while i < layers_size - 1:
-		weights_size += layers[i] * layers[i + 1]; i += 1
-	var neurons_size : int = bias_size + layers[0]
 	
 	device = RenderingServer.create_local_rendering_device()
 	var shader = device.shader_create_from_spirv(load("res://addons/neural_network/GPUNNET.glsl").get_spirv())
@@ -51,9 +44,9 @@ func _init(layers_construction: Array[int] = [1,1], learning_rate_value: float =
 	random_weights.resize(x_component * (y_component ** 2) * 4)
 	i = 0; while i < x_component * (y_component ** 2): random_weights.encode_float(i * 4, randf_range(-1.0, 1.0)); i += 1
 	
-	common_data            = device.storage_buffer_create(4                                * 4)
-	bias_storage           = device.storage_buffer_create(bias_size * int(use_bias)        * 4)
-	deltas_storage         = device.storage_buffer_create(x_component * y_component        * 4)
+	common_data            = device.storage_buffer_create(4                            * 4 + 1)
+	bias_storage           = device.storage_buffer_create((x_component - 1) * y_component  * int(use_bias) * 4)
+	deltas_storage         = device.storage_buffer_create((x_component - 1) * y_component        * 4)
 	weights_storage        = device.storage_buffer_create(x_component * (y_component ** 2) * 4, random_weights)
 	structure_storage      = device.storage_buffer_create(layers_size                      * 4)
 	output_neurons_storage = device.storage_buffer_create(x_component * y_component        * 4)
@@ -70,10 +63,12 @@ func _init(layers_construction: Array[int] = [1,1], learning_rate_value: float =
 	
 	var uniform_set = device.uniform_set_create(uniforms, shader, 0)
 	
+	fill_data()
+	
 	var compute_list = device.compute_list_begin()
 	device.compute_list_bind_compute_pipeline( compute_list, pipeline                   )
 	device.compute_list_bind_uniform_set(      compute_list, uniform_set, 0             )
-	device.compute_list_dispatch(              compute_list, x_component, y_component, 1)
+	device.compute_list_dispatch(              compute_list, x_component - 1, y_component, 1)
 	device.compute_list_end()
 
 func initialize_uniform(rid : RID, binding : int) -> RDUniform:
@@ -87,9 +82,10 @@ func fill_data() -> void:
 	var common_data_pba : PackedByteArray = PackedByteArray([])
 	common_data_pba.resize(16)
 	common_data_pba.encode_float( 0, learning_rate )
-	common_data_pba.encode_s32(   4, last_layer    )
-	common_data_pba.encode_s32(   8, layers_size   )
-	common_data_pba.encode_s32(  12, run           )
+	common_data_pba.encode_u32(   4, last_layer    )
+	common_data_pba.encode_u32(   8, layers_size   )
+	common_data_pba.encode_u8 (  12, is_bias_used  )
+	common_data_pba.encode_u32(  13, run           )
 	device.buffer_update(common_data, 0, 16, common_data_pba)
 	
 	var layers_pba : PackedByteArray = PackedByteArray([]); layers_pba.resize(layers_size * 4)
