ad207989db7ec93bceaaeadefc7a373af27a7f43
LikVis <143567019+ZeroLikviz@users.noreply.github.com>
2024-02-13 21:24:35 +0300

    optimized
    I changed structure CommonData so CPU can provide me pre-converted to float variable and thus I dont have to do it on GPU

--- addons/neural_network/GPUNNET.glsl
+++ addons/neural_network/GPUNNET.glsl
@@ -12,7 +12,7 @@ layout(set = 0, binding = 0, std430) restrict buffer CommonData {
 	float learning_rate;
 	uint  last_layer;
 	uint layers_size;
-	bool bias;
+	float bias;
 	uint mode;
 } data;
 
@@ -59,7 +59,7 @@ void main() {
 			x += 1;
 			if(y < layers.data[x])
 			{
-				output_neurons.data[x][y] = biases.data[gl_WorkGroupID.x][y] * float(data.bias);
+				output_neurons.data[x][y] = biases.data[gl_WorkGroupID.x][y] * data.bias;
 				for(mediump uint i = 0; i < layers.data[x - 1]; ++i)
 				{
 					output_neurons.data[x][y] += output_neurons.data[x - 1][i] * weights.data[x - 1][i][y];
