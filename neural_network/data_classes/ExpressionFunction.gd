1b9d34ae7121b888815607f75af3519b6387a244
LikVis <143567019+ZeroLikviz@users.noreply.github.com>
2025-06-03 02:15:46 +0300

    Update ExpressionFunction.gd

--- neural_network/data_classes/ExpressionFunction.gd
+++ neural_network/data_classes/ExpressionFunction.gd
@@ -7,5 +7,5 @@ func _init(prototype: String, expression: String) -> void:
 	self.expression = expression
 	var gd_file : GDScript = GDScript.new()
 	gd_file.source_code = "static var function : Callable = " + prototype + expression.indent("\t")
-	gd_file.reload()
+	gd_file.reload(false)
 	callable = gd_file.function
