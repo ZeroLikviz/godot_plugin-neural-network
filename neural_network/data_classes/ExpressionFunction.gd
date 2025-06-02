a08b02f9fc8b578e694b77bfdfeb7afa44bb769b
LikVis <143567019+ZeroLikviz@users.noreply.github.com>
2025-06-02 15:23:30 +0300

    Create ExpressionFunction.gd

---
+++ neural_network/data_classes/ExpressionFunction.gd
@@ -0,0 +1,11 @@
+class_name ExpressionFunction
+
+var callable: Callable
+var expression: String
+
+func _init(prototype: String, expression: String) -> void:
+	self.expression = expression
+	var gd_file : GDScript = GDScript.new()
+	gd_file.source_code = "static var function : Callable = " + prototype + expression.indent("\t")
+	gd_file.reload()
+	callable = gd_file.function
